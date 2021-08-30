cd6898fddaf4bc524e0054afacbcb66adde4051d
lan.yu <lan.yu@changhong.com>
2021-08-30 15:29:49 +0800

    [Add]: temp文件夹

---
+++ src/views/temp/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script>
+import Ajax from '@/assets/js/api/ajax.js';
+import HelloWorld from '@/components/HelloWorld';
+
+export default {
+  name: 'Home',
+  data () {
+    return {};
+  },
+  components: {
+    HelloWorld
+  },
+  mounted () {
+    Ajax(`/tui/commission`, {
+      'id': 222
+    }).then(res => {
+      console.log(res);
+      console.log('1111111111');
+    });
+  }
+};
+</script>
+
+<style>
+
+</style>
